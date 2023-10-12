07f0b29188b14f425fd699a1a73398ca3c291e0e
SilasPerillo <silasperillo@gmail.com>
2023-10-12 12:18:14 -0300

    feat: create list deputies, migration

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
+Route::get('/', function () { // não estou usando
     return view('welcome');
+});
+
+Route::get('/deputies/create-list', function () { // esta rota esta sendo usada apenas para gerar o array de deputados
+    $url = 'https://dadosabertos.almg.gov.br/ws/legislaturas/19/deputados/em_exercicio?formato=json';
+
+    $response = Http::get($url);
+
+    $list = json_decode($response->body())->list;
+    $newArray = [];
+
+    foreach ($list as $elem) {
+        $aux = new stdClass();
+        $aux->id = $elem->id;
+        $aux->nome = $elem->nome;
+        array_push($newArray, $aux);
+    }
+
+    return $newArray;
 });
